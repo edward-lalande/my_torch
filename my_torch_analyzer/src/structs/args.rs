932a7c90b2939cf0a3fa8a3bae7938c702e5a938
edward-lalande <edward.lalande@epitech.eu>
2026-06-01 16:08:23 +0900

    fix(AnalyzerArgs): modifying arguments that the program takes to be more coherent with the project

--- my_torch_analyzer/src/structs/args.rs
+++ my_torch_analyzer/src/structs/args.rs
@@ -1,18 +1,20 @@
 use std::fmt;
 
 pub struct TorchArgs {
-    pub model: String,
-    pub input: u64,
+    pub is_predict: bool,
+    pub is_train: bool,
     pub save_file: String,
+    pub load_file: String,
     pub input_file: String,
 }
 
 impl TorchArgs {
     pub fn new() -> Self {
         Self {
-            model: String::new(),
-            input: 0,
+            is_predict: false,
+            is_train: false,
             save_file: String::new(),
+            load_file: String::new(),
             input_file: String::new(),
         }
     }
@@ -22,8 +24,8 @@ impl fmt::Debug for TorchArgs {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
             f,
-            "TorchArgs {{ model: {}, input: {}, save_file: {}, input_file: {} }}",
-            self.model, self.input, self.save_file, self.input_file
+            "TorchArgs {{ is_predict: {}, is_train: {}, save_file: {}, load_file: {}, input_file: {} }}",
+            self.is_predict, self.is_train, self.save_file, self.load_file, self.input_file
         )
     }
 }
