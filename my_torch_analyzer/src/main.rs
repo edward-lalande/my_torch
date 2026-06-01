7259edfc3276aa0cbda9855d3b5231e651456fc5
edward-lalande <edward.lalande@epitech.eu>
2026-06-01 16:08:44 +0900

    feat(Analyzer): argument handling with the print of usage when it's not good

--- my_torch_analyzer/src/main.rs
+++ my_torch_analyzer/src/main.rs
@@ -2,24 +2,21 @@ use std::env;
 use std::process::exit;
 mod structs;
 use structs::args::*;
-
-use crate::structs::perceptron::Perceptron;
+pub mod utils;
 
 fn parse_args(args: Vec<String>) -> TorchArgs {
     let mut torch_args = TorchArgs::new();
     let mut iter = args.into_iter();
 
+    iter.next(); // to jump the binary file
+
     while let Some(arg) = iter.next() {
         match arg.as_str() {
-            "--new" => {
-                if let Some(val) = iter.next() {
-                    torch_args.input = val.parse().unwrap_or(0);
-                }
+            "--predict" => {
+                torch_args.is_predict = true;
             }
-            "--load" => {
-                if let Some(val) = iter.next() {
-                    torch_args.model = val;
-                }
+            "--train" => {
+                torch_args.is_train = true;
             }
             "--save" => {
                 if let Some(val) = iter.next() {
@@ -28,7 +25,10 @@ fn parse_args(args: Vec<String>) -> TorchArgs {
             }
 
             _ => {
-                torch_args.input_file = arg;
+                torch_args.load_file = arg;
+                if let Some(val) = iter.next() {
+                    torch_args.input_file = val;
+                }
             }
         }
     }
@@ -39,20 +39,47 @@ fn parse_args(args: Vec<String>) -> TorchArgs {
 fn main() {
     let args: Vec<String> = env::args().collect();
     let torch_args = parse_args(args);
-    if !torch_args.model.is_empty() && torch_args.input != 0 {
-        println!("Impossible to have a load file and create a new one");
+    println!("is_predict: {}", torch_args.is_predict);
+    println!("is_train: {}", torch_args.is_train);
+    println!("save_file.is_empty(): {}", torch_args.save_file.is_empty());
+    println!("load_file.is_empty(): {}", torch_args.load_file.is_empty());
+    println!(
+        "input_file.is_empty(): {}",
+        torch_args.input_file.is_empty()
+    );
+    if ((torch_args.is_predict && torch_args.is_train)
+        || (!torch_args.is_predict && !torch_args.is_train))
+        || (torch_args.is_train && torch_args.save_file.is_empty())
+        || (torch_args.load_file.is_empty() || torch_args.input_file.is_empty())
+    {
+        println!("USAGE");
+        println!(
+            "\t./my_torch_analyzer [--predict | --train [--save SAVEFILE]] LOADFILE CHESSFILE\n"
+        );
+        println!("DESCRIPTION");
+        println!(
+            "\t--train\tLaunch the neural network in training mode. Each chessboard in FILE must"
+        );
+        println!(
+            "\t\tcontain inputs to send to the neural network in FEN notation and the expected output"
+        );
+        println!(
+            "\t\tseparated by space. If specified, the newly trained neural network will be saved in"
+        );
+        println!("\t\tSAVEFILE. Otherwise, it will be saved in the original LOADFILE.");
+        println!(
+            "\t--predict\tLaunch the neural network in prediction mode. Each chessboard in FILE must"
+        );
+        println!(
+            "\t\tcontain inputs to send to the neural network in FEN notation, and optionally an expected"
+        );
+        println!("\t\toutput.");
+        println!("\t--save\tSave neural network into SAVEFILE. Only works in train mode.\n");
+        println!("\tLOADFILE\tFile containing an artificial neural network");
+        println!("\tCHESSFILE\tFile containing chessboards");
+
         exit(1);
     }
-    let inputs: Vec<(f64, f64)> = vec![(0.0, 0.0), (1.0, 0.0), (0.0, 1.0), (1.0, 1.0)];
-    let outputs: Vec<f64> = vec![0.0, 0.0, 0.0, 1.0];
-    let mut perceptron: Perceptron = Perceptron::perceptron_from_file(torch_args.model);
 
-    perceptron.fit(inputs, outputs);
-
-    println!("Perceptron parameters: {:?}", perceptron);
-
-    println!("0 & 0 = {}", perceptron.predict((0.0, 0.0))); // Must show 0
-    println!("1 & 0 = {}", perceptron.predict((1.0, 0.0))); // Must show 0
-    println!("0 & 1 = {}", perceptron.predict((0.0, 1.0))); // Must show 0
-    println!("1 & 1 = {}", perceptron.predict((1.0, 1.0))); // Must show 1
+    println!("{:?}", torch_args);
 }
