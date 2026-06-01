12894057c1293d2f77e25c91fcba953510c9e6e3
edward-lalande <edward.lalande@epitech.eu>
2026-06-02 02:32:23 +0900

    feat(Layer): forward method to calcul the output of the neuron, comput output delta to compute the delta output of each neurons of the layers with the target, compute hidden deltas to calcul the delta on hidder layers and comput them and update layer weights for update the neurons weight and bias with the input

--- my_torch_analyzer/src/structs/layers.rs
+++ my_torch_analyzer/src/structs/layers.rs
@@ -1,8 +1,14 @@
+use std::process;
+
+use serde::{Deserialize, Serialize};
+
 use crate::structs::neuron::Neuron;
 
+#[derive(Serialize, Deserialize)]
 pub struct Layers {
     neurons: Vec<Neuron>,
 }
+
 impl Layers {
     pub fn new(weights_size: u32, learning_rate: f64, hidden_neurons: u32) -> Layers {
         let mut neurons: Vec<Neuron> = Vec::new();
@@ -14,5 +20,43 @@ impl Layers {
         Layers { neurons }
     }
 
-    pub fn activation_function(&mut self) {}
+    pub fn forward_layer(&mut self, inputs: &Vec<f64>) -> Vec<f64> {
+        let mut sum_output: Vec<f64> = Vec::with_capacity(self.neurons.len());
+
+        for neuron in self.neurons.iter_mut() {
+            neuron.calcul_output(inputs);
+            sum_output.push(neuron.output);
+        }
+
+        sum_output
+    }
+
+    pub fn compute_output_deltas(&mut self, targets: &Vec<f64>) {
+        if self.neurons.len() != targets.len() {
+            eprintln!("Neurons and target output don't have the same len.");
+            process::exit(1);
+        }
+
+        for (neuron, target) in self.neurons.iter_mut().zip(targets.iter()) {
+            neuron.calcul_delta_output(target);
+        }
+    }
+
+    pub fn compute_hidden_deltas(&mut self, next_layer: &Layers) {
+        for i in 0..self.neurons.len() {
+            let mut upstream_error: f64 = 0.0;
+
+            for j in 0..next_layer.neurons.len() {
+                let next_neuron = &next_layer.neurons[j];
+                upstream_error += next_neuron.weights[i] * next_neuron.delta;
+            }
+            self.neurons[i].calcul_delta_hidden(upstream_error);
+        }
+    }
+
+    pub fn update_layer_weights(&mut self, inputs: &Vec<f64>) {
+        for neuron in self.neurons.iter_mut() {
+            neuron.update_weight_and_bias(inputs);
+        }
+    }
 }
